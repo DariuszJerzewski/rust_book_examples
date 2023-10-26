975b76779d99142a2903aa76797e81c9f9ff9dae
Dariusz Jerzewski <djerzewski@protonmail.com>
2023-10-26 23:30:46 +0200

    chapter 5 structs - on Using Tuple Structs end of page 84

--- 05_01_defining_and_instatiating_structs/src/main.rs
+++ 05_01_defining_and_instatiating_structs/src/main.rs
@@ -1,14 +1,33 @@
-
 struct User {
     active: bool,
     username: String,
     email: String,
-    sign_in_count: u64
+    sign_in_count: u64,
 }
-
 
 fn main() {
-    
+    let mut user = User {
+        active: true,
+        username: String::from("zryw"),
+        email: String::from("zryw@zryw.zryw"),
+        sign_in_count: 0,
+    };
+
+    user.sign_in_count = 1;
+
+    let user = create_user("test", "email");
+
+    let user = User {
+        username: String::from("new"),
+        ..user
+    };
 }
 
-
+fn create_user(username: &str, email: &str) -> User {
+    User {
+        active: true,
+        username: String::from(username),
+        email: String::from(email),
+        sign_in_count: 0,
+    }
+}
